a5559356408838f42c036c2b44cdb20258c4d1f6
TWL <2405077320@qq.com>
2024-08-13 15:53:34 +0800

    修改字典维护页面

--- src/main/java/com/rrm/module/dict/mapper/RrmDictTypeMapper.java
+++ src/main/java/com/rrm/module/dict/mapper/RrmDictTypeMapper.java
@@ -6,11 +6,12 @@ import com.rrm.module.dict.domain.model.RrmDictType;
 import com.rrm.module.dict.domain.vo.RrmDictTypeVO;
 import com.rrm.module.dict.dto.RrmDictTypeDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface RrmDictTypeMapper extends BaseMapper<RrmDictType> {
 
-    IPage<RrmDictTypeVO> selectDictTypePage(IPage<?> page, RrmDictTypeDTO dto);
+    IPage<RrmDictTypeVO> selectDictTypePage(IPage<?> page, @Param("dto") RrmDictTypeDTO dto);
 
     RrmDictType selectByTypeCode(String typeCode);
 }
