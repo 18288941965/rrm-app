8d9b9f44191fa3185787a00d90280ba33c684af6
Administrator <18288941965@163.com>
2024-09-01 17:51:36 +0800

    表结构修改

--- doc/db/rrm.sql
+++ doc/db/rrm.sql
@@ -77,19 +77,24 @@ create table rrm.rrm_menu_element
 
 create table rrm.rrm_resource
 (
-    id             varchar(36)       not null comment '主键',
+    id             varchar(36)       not null comment '主键'
+        primary key,
+    service_name   varchar(255)      not null comment '服务名称',
+    package_name   varchar(255)      not null comment '包名',
     class_name     varchar(255)      not null comment '类名',
+    method_name    varchar(100)      not null comment '方法名',
     request_path   varchar(500)      not null comment '请求路径',
     request_method varchar(20)       not null comment '请求方式',
-    name           varchar(255)      not null comment '资源名称',
-    type           varchar(2)        null comment '资源类型(字典)',
+    resource_name  varchar(255)      not null comment '资源名称',
+    resource_type  varchar(2)        null comment '资源类型(字典)',
     auth_code      int               not null comment '授权码',
+    environment    varchar(2)        null comment '资源环境（dev,production）',
     created_by     int               not null comment '创建人',
     created_at     timestamp(6)      not null comment '创建时间',
     updated_by     int               null comment '更新人',
     updated_at     timestamp(6)      null comment '更新时间',
     status         tinyint default 1 not null comment '状态(1：启用，0：停用)',
-    item_code      varchar(2)        not null comment '项目代码'
+    item_code      varchar(6)        not null comment '项目代码'
 )
     comment '资源管理';
 
@@ -111,11 +116,11 @@ create table rrm.rrm_role
 
 create table rrm.rrm_user
 (
-    id       int auto_increment comment '主键'
+    id          int auto_increment comment '主键'
         primary key,
-    username varchar(50)  not null comment '账号',
-    password varchar(50)  not null comment '密码',
-    comment  varchar(255) null comment '备注',
+    username    varchar(50)  not null comment '账号',
+    password    varchar(50)  not null comment '密码',
+    description varchar(255) null comment '用户描述',
     constraint i_username
         unique (username)
 );
